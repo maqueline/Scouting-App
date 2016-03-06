48f0f265e74477e0ff1afe8312dd6472aec225e9
Evë Maquelin <maqueline@catlin.edu>
2016-03-05 18:08:06 -0800

    added cordova file plugin

--- www/js/index.js
+++ www/js/index.js
@@ -82,31 +82,60 @@ function login(form) {
     //     persist.users = JSON.parse(response);
     // });
     // console.log(persist.users);
-    var json = {
-    "i1": "Ross Geller",
-    "i2": "Monica Geller-Bing",
-    "i3": "Chandler Bing",
-    "i11": "Jake Springer",
-    "i13": "Evë Maquelin",
-    "i17": "Adolfo Apolloni",
-    "i18": "Ryan Selden",
-    "i21": "Tyler Nguyen",
-    "i22": "Robin Attey",
-    "i33": "Alexander Mackworth",
-    "i37": "Kobi Saha",
-    "i42": "Cel Skeggs",
-    "i96": "Liam Bendicksen",
-    "i98": "Iman Wahle",
-    "i99": "Aidan Smith"
-};
+    var json = `{
+    "i0":"Systest - Ryan Selden",
+    "i00":"Systest - Ryan Selden",
+    "i1":"SystestMain",
+    "i2":"Systest - Evë Maquelin",
+    "i3":"Systest - Gregor Peach",
+    "i4":"Systest - David Vollum",
+    "i11":"Jake Springer",
+    "i12":"Solomon Olshin",
+    "i13":"Evë Maquelin",
+    "i14":"Ben Balden",
+    "i15":"Jake Hansen",
+    "i16":"Ben Johnson",
+    "i17":"Adolfo Apolloni",
+    "i18":"Ryan Selden",
+    "i19":"Culla Galaher",
+    "i21":"Tyler Nguyen",
+    "i22":"Robin Attey",
+    "i23":"Hammad Khan",
+    "i24":"David Liu",
+    "i25":"Josephine Evans",
+    "i27":"Jacob Bendicksen",
+    "i32":"Mathus Leungpathomaram",
+    "i33":"Alexander Mackworth",
+    "i37":"Kobi Saha",
+    "i42":"Cel Skeggs",
+    "i44":"Hannah Fisher",
+    "i45":"Noor Wahle",
+    "i47":"Iris Ellenberg",
+    "i48":"Natalie Dodson",
+    "i51":"Claire Rosenfeld",
+    "i52":"Nicolas Springer",
+    "i58":"Quinn Okabayashi",
+    "i61":"Calissa Spooner",
+    "i66":"Anna Dodson",
+    "i68":"Jasper Gordon",
+    "i69":"Stop Wasting Time. Go scout!!!",
+    "i73":"Fin Hoyer",
+    "i77":"Gregor Peach",
+    "i86":"Amber Merrill",
+    "i96":"Liam Bendicksen",
+    "i97":"David Vollum",
+    "i98":"Iman Wahle",
+    "i99":"Aidan Smith"
+}`
 
-    //persist.people = JSON.parse(json);
-    persist.people = json;
+    persist.people = JSON.parse(json);
+    //persist.people = json;
 
     var ID = "i" + String(form.inputID.value);
     console.log(ID);
 
     if (typeof persist.people[ID] == "string") {
+        console.log("hello");
         persist.user.name = persist.people[ID]; // logindata.get(ID); or something
         persist.user.id = String(form.inputID.value);
 
@@ -138,44 +167,80 @@ function sendOff(spec) {
 }
 
 function onDeviceReady() { //request the persistent file system
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFSSuccess, onError);
-    logit("hello");
+    function readFromFile(fileName, cb) {
+        var pathToFile = cordova.file.dataDirectory + fileName;
+        window.resolveLocalFileSystemURL(pathToFile, function (fileEntry) {
+            fileEntry.file(function (file) {
+                var reader = new FileReader();
+
+                reader.onloadend = function (e) {
+                    cb(JSON.parse(this.result));
+                };
+
+                reader.readAsText(file);
+            }, errorHandler.bind(null, fileName));
+        }, errorHandler.bind(null, fileName));
+    }
+
+    var fileData;
+    readFromFile('data.json', function (data) {
+        fileData = data;
+        console.log(fileData);
+    });
+
+    function writeToFile(fileName, data) {
+        data = JSON.stringify(data, null, '\t');
+        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+            directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
+                fileEntry.createWriter(function (fileWriter) {
+                    fileWriter.onwriteend = function (e) {
+                        // for real-world usage, you might consider passing a success callback
+                        console.log('Write of file "' + fileName + '"" completed.');
+                    };
+
+                    fileWriter.onerror = function (e) {
+                        // you could hook this up with our global error handler, or pass in an error callback
+                        console.log('Write failed: ' + e.toString());
+                    };
+
+                    var blob = new Blob([data], { type: 'text/plain' });
+                    fileWriter.write(blob);
+                }, errorHandler.bind(null, fileName));
+            }, errorHandler.bind(null, fileName));
+        }, errorHandler.bind(null, fileName));
+    }
+
+    writeToFile('example.json', { foo: 'bar' });
+
+    var errorHandler = function (fileName, e) {  
+        var msg = '';
+
+        switch (e.code) {
+            case FileError.QUOTA_EXCEEDED_ERR:
+                msg = 'Storage quota exceeded';
+                break;
+            case FileError.NOT_FOUND_ERR:
+                msg = 'File not found';
+                break;
+            case FileError.SECURITY_ERR:
+                msg = 'Security error';
+                break;
+            case FileError.INVALID_MODIFICATION_ERR:
+                msg = 'Invalid modification';
+                break;
+            case FileError.INVALID_STATE_ERR:
+                msg = 'Invalid state';
+                break;
+            default:
+                msg = 'Unknown error';
+                break;
+        };
+
+        console.log('Error (' + fileName + '): ' + msg);
+    }
 }
 
 function init() {
     document.addEventListener("deviceready", onDeviceReady, true);
     logit("hi");
-}
-
-function onFSSuccess(fs) { 
-    fileSystem = fs;
-    getById("#dirListingButton").addEventListener("touchstart", doDirectoryListing);
-    getById("#addFileButton").addEventListener("touchstart", doAppendFile);
-    getById("#readFileButton").addEventListener("touchstart", doReadFile);
-    getById("#metadataFileButton").addEventListener("touchstart", doMetadataFile);
-    getById("#deleteFileButton").addEventListener("touchstart", doDeleteFile);
-    logit("Got the file system: "+fileSystem.name +"<br/>" + "root entry name is "+fileSystem.root.name + "<p/>");
-    doDirectoryListing();
-}
-
-function gotFiles(entries) {
-    var s = "";
-    for (var i = 0, len = entries.length; i < len; i++) { 
-        //entry objects include: isFile, isDirectory, name, fullPath
-        s += entries[i].fullPath;
-        if (entries[i].isFile) {
-            s += " [F]";
-        } else { 
-            s += " [D]";
-        } 
-        s += "<br/>";
-    } 
-    s += "<p/>";
-    logit(s);
-} 
-
-function doDirectoryListing(e) { 
-    //get a directory reader from our FS 
-    var dirReader = fileSystem.root.createReader();
-    dirReader.readEntries(gotFiles,onError);
 }
