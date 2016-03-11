a805668be645551fbfb266e31f3ebb8ffc1b0508
Evë Maquelin <maqueline@catlin.edu>
2016-03-11 00:11:42 -0800

    help

--- www/js/index.js
+++ www/js/index.js
@@ -166,7 +166,86 @@ function sendOff(spec) {
     console.log(spec);
 }
 
+<<<<<<< HEAD
+=======
+function onDeviceReady() { //request the persistent file system
+    function readFromFile(fileName, cb) {
+        var pathToFile = cordova.file.dataDirectory + fileName;
+        window.resolveLocalFileSystemURL(pathToFile, function (fileEntry) {
+            fileEntry.file(function (file) {
+                var reader = new FileReader();
+
+                reader.onloadend = function (e) {
+                    cb(JSON.parse(this.result));
+                };
+
+                reader.readAsText(file);
+            }, errorHandler.bind(null, fileName));
+        }, errorHandler.bind(null, fileName));
+    }
+
+    var fileData;
+    readFromFile('data.json', function (data) {
+        fileData = data;
+        console.log(fileData);
+    });
+
+    function writeToFile(fileName, data) {
+        data = JSON.stringify(data, null, '\t');
+        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+            directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
+                fileEntry.createWriter(function (fileWriter) {
+                    fileWriter.onwriteend = function (e) {
+                        // for real-world usage, you might consider passing a success callback
+                        console.log('Write of file "' + fileName + '"" completed.');
+                    };
+
+                    fileWriter.onerror = function (e) {
+                        // you could hook this up with our global error handler, or pass in an error callback
+                        console.log('Write failed: ' + e.toString());
+                    };
+
+                    var blob = new Blob([data], { type: 'text/plain' });
+                    fileWriter.write(blob);
+                }, errorHandler.bind(null, fileName));
+            }, errorHandler.bind(null, fileName));
+        }, errorHandler.bind(null, fileName));
+    }
+
+    writeToFile('beebalm.json', { foo: 'bar' });
+
+    var errorHandler = function (fileName, e) {  
+        var msg = '';
+
+        switch (e.code) {
+            case FileError.QUOTA_EXCEEDED_ERR:
+                msg = 'Storage quota exceeded';
+                break;
+            case FileError.NOT_FOUND_ERR:
+                msg = 'File not found';
+                break;
+            case FileError.SECURITY_ERR:
+                msg = 'Security error';
+                break;
+            case FileError.INVALID_MODIFICATION_ERR:
+                msg = 'Invalid modification';
+                break;
+            case FileError.INVALID_STATE_ERR:
+                msg = 'Invalid state';
+                break;
+            default:
+                msg = 'Unknown error';
+                break;
+        };
+
+        console.log('Error (' + fileName + '): ' + msg);
+    }
+}
+
+>>>>>>> parent of 0225753... file testing ft. icenium
 function init() {
+    cd('root/sdcard').write('myText.txt','My foobar is great.');
     document.getElementById("blip").innerHTML = cordova.file.dataDirectory;
     document.addEventListener("deviceready", onDeviceReady, true);
+    writeToFile('ininin.json', { foo: 'bar' });
 }
